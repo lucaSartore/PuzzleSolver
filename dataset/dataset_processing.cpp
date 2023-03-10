b96dd1c7ef0b2d2e875920d1acfa043eb4dcb616
lucaSartore <lucasar.com8@gmail.com>
2023-03-10 15:28:20 +0100

    add structure

---
+++ dataset/dataset_processing.cpp
@@ -0,0 +1,19 @@
+/*
+ * This program is created to process the images,
+ *
+ * he has to:
+ *      - apply a threshold to the image
+ *      - split the images into single pieces
+ *      - find the corners
+ *      - save the information for later
+ * */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
