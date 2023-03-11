bcb98cefbdd02a3b74ffa47bdaa93b02d8592926
luca <lucasar.com8@gmail.com>
2023-03-11 19:52:26 +0100

    thrsholding working

--- dataset/dataset_processing.cpp
+++ dataset/dataset_processing.cpp
@@ -15,8 +15,8 @@ using namespace std;
 using namespace cv;
 
 // the directory the program will use as a destination
-#define DIRECTORY "cat_500pcs"
-#define IMAGE_FORMAT ".jpeg"
+#define DIRECTORY "blue_500pcs"
+#define IMAGE_FORMAT ".jpg"
 int main(){
 
     // list for keeping the files
@@ -40,21 +40,59 @@ int main(){
         }
         raw_images.push_back(img);
         c++;
+        break;
     }
 
     // step 1: converting all the images to gray
     vector<Mat> mask_images = vector<Mat>();
     for(auto raw_image: raw_images){
+        // converting image to gray
         Mat gray_image;
         cvtColor(raw_image, gray_image, COLOR_BGR2GRAY);
+        // applying a threshold
         Mat threshold_image;
-        //threshold(gray_image,threshold,);
-        //Canny( gray_image, threshold_image, 100, 200 );
-        threshold(gray_image, threshold_image, 200, 255, THRESH_BINARY);
-        mask_images.push_back(threshold_image);
-    }
+        threshold(gray_image, threshold_image, 25, 255, THRESH_BINARY);
 
-    imshow("test", mask_images[0]);
+
+        // create a matrix kernel full of 255
+        int kernel_data[20*20];
+        for(auto &i: kernel_data){i=255;}
+        Mat kernel = Mat(20,20,CV_8U, kernel_data);
+
+        // remove small white dots
+        Mat white_dots_removed_image;
+        morphologyEx(threshold_image,white_dots_removed_image,MORPH_OPEN,kernel);
+
+        // create a matrix kernel full of 255
+        int kernel_data2[12*12];
+        for(auto &i: kernel_data){i=255;}
+        Mat kernel2 = Mat(12,12,CV_8U, kernel_data2);
+
+        // remove black white dots
+        Mat black_dots_removed_image;
+        morphologyEx(white_dots_removed_image,black_dots_removed_image,MORPH_CLOSE,kernel2);
+
+        mask_images.push_back(black_dots_removed_image);
+    }
+    //Mat resized;
+    //resize(mask_images[0],resized,Size(70,100));
+
+    Mat individual_pieces;
+    int number_of_pieces = connectedComponents(mask_images[0],individual_pieces);
+
+    cout << individual_pieces.depth();
+
+    Mat new_dype;
+    individual_pieces.convertTo(new_dype,CV_8U);
+
+
+    Mat resized;
+    resize(new_dype,resized,Size(700,1000));
+
+
+    Mat test = resized == 22;
+
+    imshow( "Connected Components", test);
     waitKey(0);
 
 
