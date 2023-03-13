427f71d53db141a4a7b9c3ca8ddd81b300d10d66
luca <lucasar.com8@gmail.com>
2023-03-13 22:46:01 +0100

    finish hole romoving code

--- dataset/dataset_processing.cpp
+++ dataset/dataset_processing.cpp
@@ -14,6 +14,7 @@
 using namespace std;
 using namespace cv;
 
+
 // the directory the program will use as a destination
 #define DIRECTORY "blue_500pcs"
 // format of the file
@@ -93,8 +94,15 @@ void split_pieces_into_single_images(){
         Mat individual_pieces, stats, center;
         int number_of_pieces = connectedComponentsWithStats(final_mask,individual_pieces,stats,center);
 
+        cout << number_of_pieces << endl;
+
+        Mat temp = individual_pieces == number_of_pieces;
+
+
         // for each individual piece
-        for(int i=1; i<=number_of_pieces; i++){
+        for(int i=1; i<number_of_pieces; i++){
+            cout << "processing piece: " << piece_index << endl;
+
             // coordinates to crop the image
             int x1 = -CROP_MARGIN + stats.at<int>(i, cv::CC_STAT_LEFT);
             int y1 = -CROP_MARGIN + stats.at<int>(i, cv::CC_STAT_TOP);
@@ -111,21 +119,25 @@ void split_pieces_into_single_images(){
                 // convert it to simpler format
                 single_piece_32U.convertTo(single_piece,CV_8U);
                 // crop it
+
+                //cout << "x1 x2 y1 y2: " << x1 << " " << x2  << " " << y1 << " " << y2 << endl;
                 Mat cropped_image = single_piece(Range(y1,y2),Range(x1,x2));
 
                 //show the piece corpped
-                /*Mat cropped_image_resized;
+                /*
+                //Mat cropped_image_resized;
                 Mat to_target_resized;
-                resize(cropped_image,cropped_image_resized,Size(400,400));
+                //resize(cropped_image, cropped_image_resized, Size(400, 400));
                 Mat to_target = raw_image;
-                rectangle(to_target,Point(x1,y1),Point(x2,y2),Scalar(255,0,0,0),30);
-                resize(to_target,to_target_resized,Size(550,800));
-                imshow( "output", cropped_image_resized);
-                imshow( "where", to_target_resized);
-                waitKey(0);*/
+                rectangle(to_target, Point(x1, y1), Point(x2, y2), Scalar(255, 0, 0, 0), 30);
+                resize(to_target, to_target_resized, Size(550, 800));
+                //imshow("output", cropped_image_resized);
+                imshow("where", to_target_resized);
+                waitKey(0);
+                */
 
                 //calculate path
-                string path = string("../") + string(DIRECTORY) + string("/divided/") + to_string(piece_index++) + string(IMAGE_FORMAT);
+                //string path = string("../") + string(DIRECTORY) + string("/divided/") + to_string(piece_index++) + string(IMAGE_FORMAT);
 
                 //save the file
                 imwrite(path,cropped_image);
@@ -138,20 +150,23 @@ void split_pieces_into_single_images(){
 // the algorithm find the filler of a piece.
 // a filler is an area that need to be included for the image to be considered convex
 // the areas that will be calculated will be considered filler only if their area (in pixels) is greater than this threshold
-#define FILLER_AREA_THRESHOLD 1000
+#define FILLER_AREA_THRESHOLD 9000
 // when a filler is found, it can be of 2 types...
 // a filler created by a hole in the puzzle piece
-// or a filler created by am extension of the puzzle piece
-// if he is created by a hole it will be a convex shape
-// i can define the "convex percentage", that is the area of the original image decided by the area of the convex hull of the image
-// than i can define that a filler is a hole if his convex percentage is lower than this percentage
-#define MIN_CONVEX_PERCENTAGE 0.9
+#define EROSION_KERNEL_SIZE 100
+#define MIN_SHRINKING_PERCENTAGE  0.44
 //this function make some preprocessing to increase the speed of the comparison operation later
 void do_images_pre_processing(){
     Mat temp;
+    Mat temp2;
+    Mat kernel;
     int piece_index = 1;
 
     while (true){
+
+        cout << "processing piece: " << piece_index << endl;
+
+
         //step 1: read all the files
         string path = string("../") + string(DIRECTORY) + string("/divided/") + to_string(piece_index) + string(IMAGE_FORMAT);
         // image with the scanned raw data
@@ -166,61 +181,133 @@ void do_images_pre_processing(){
                 break;
             }
         }
-        piece_index++;
 
         // find the convex hull of the original mask
         Mat convex_hull;
         quick_convex_hull(piece,convex_hull);
 
+        // do a first erosion, to remove the thin area close to the puzzle hole
+        Mat convex_hull_erosion;
+        kernel = Mat::zeros(Size(EROSION_KERNEL_SIZE,EROSION_KERNEL_SIZE),CV_8U) == 0;
+        erode(convex_hull,convex_hull_erosion,kernel);
+
         // the filler pieces that can make the original image a convex shape
         Mat filler;
         bitwise_xor(convex_hull,piece,filler);
 
-        //erode the border of the image to remove thin line
+        //erode and expand the border of the image to remove thin line
         Mat filler_eroded;
-        Mat kernel = Mat::zeros(Size(6,6),CV_8U) == 0;
+        kernel = Mat::zeros(Size(6,6),CV_8U) == 0;
         erode(filler,filler_eroded,kernel);
+        Mat kernel2 = Mat::zeros(Size(6+1,6+1),CV_8U) == 0;
+        dilate(filler_eroded,filler,kernel2);
 
-        //resize(filler,temp,Size(500,500));
-        //imshow("",temp);
-        //waitKey(0);
 
         // split the filler into single piece
         Mat filler_split;
-        int number_of_pieces = connectedComponents(filler_eroded,filler_split);
-
+        int number_of_pieces = connectedComponents(filler,filler_split);
         // a copy of the original piece with removed holes
         Mat piece_no_hole = piece.clone();
 
         // decide if every piece is a hole or not, if is a hole i fill it up
-        for (int i=1; i<=number_of_pieces; ++i){
+        for (int i=1; i<number_of_pieces; ++i){
             Mat single_filler = filler_split == i;
 
-            // see comment on FILLER_AREA_THRESHOLD
-            if(countNonZero(single_filler) > FILLER_AREA_THRESHOLD){
-                Mat single_filler_convex_hull;
-                quick_convex_hull(single_filler,single_filler_convex_hull);
+            // ckeck that the filler is not just a thinly line due to a broder imperfection
+            Mat remaining_filler;
+            bitwise_and(convex_hull_erosion,single_filler,remaining_filler);
 
+            // see comment on FILLER_AREA_THRESHOLD
+            if(countNonZero(single_filler) > FILLER_AREA_THRESHOLD && countNonZero(remaining_filler) > 0){
+                // calculating the distance from the most distance pixel in a filler and the border
+                int last_zero_found = INT_MAX;
+                int distance = 100;
+                int distance_prev = 0;
+                int last_non_zero_found = 0;
+                while (distance != distance_prev){
+                    distance_prev = distance;
+                    Mat convex_hull_erosion_temp;
+                    kernel = Mat::zeros(Size(distance,distance),CV_8U) == 0;
+                    erode(convex_hull_erosion,convex_hull_erosion_temp,kernel);
+
+                    // mask containing the pixels at a distance grater then `distance`
+                    Mat masked_filler;
+                    bitwise_and(convex_hull_erosion_temp,single_filler,masked_filler);
+
+                    // display
+                    /*resize(masked_filler,temp,Size(500,500));
+                    resize(convex_hull_erosion_temp,temp2,Size(500,500));
+                    imshow("1",temp);
+                    imshow("2",temp2);
+                    waitKey(0);*/
+
+
+                    // binary searcher
+                    if (countNonZero(masked_filler) > 0){
+                        // distance is to small
+                        last_non_zero_found = distance;
+                        if(last_zero_found == INT_MAX){
+                            distance*=2;
+                        }else{
+                            distance = (distance+last_zero_found)/2;
+                        }
+                    }else{
+                        // distance is too big
+                        last_zero_found = distance;
+                        distance = (distance + last_non_zero_found)/2;
+                    }
+                }
+
+                // an erosion with the kernel that is half of the calculated distance
+                Mat convex_hull_half_erosion;
+                kernel = Mat::zeros(Size(distance/2,distance/2),CV_8U) == 0;
+                erode(convex_hull_erosion,convex_hull_half_erosion,kernel);
+
+
+
+                Mat single_filler_erosion_1, single_filler_erosion_2;
+                bitwise_and(single_filler,convex_hull_erosion,single_filler_erosion_1);
+                bitwise_and(single_filler,convex_hull_half_erosion,single_filler_erosion_2);
+                // nota fai 2 shringk uno dopo l'altro, e fai il controllo tra le proporzioni solo tra il secondo e il terzo
 
                 // see comment of MIN_CONVEX_PERCENTAGE for explanation
-                float convex_percentage = (float)countNonZero(single_filler)/ (float)countNonZero(single_filler_convex_hull);
-                if(convex_percentage < MIN_CONVEX_PERCENTAGE){
+                float shrinking_percentage = (float)countNonZero(single_filler_erosion_2)/ (float)countNonZero(single_filler_erosion_1);
+
+                //cout << shrinking_percentage << endl;
+
+                /*resize(single_filler_erosion_1,temp,Size(500,500));
+                resize(single_filler_erosion_2,temp2,Size(500,500));
+                imshow("1",temp);
+                imshow("2",temp2);
+                waitKey(0);*/
+
+                if(shrinking_percentage > MIN_SHRINKING_PERCENTAGE){
                     bitwise_or(piece_no_hole,single_filler,temp);
                     piece_no_hole = temp;
                 }
             }
-
         }
-
-        resize(piece_no_hole,temp,Size(500,500));
+        /*resize(piece_no_hole,temp,Size(500,500));
         imshow("",temp);
-        waitKey(0);
+        waitKey(0);*/
 
-
+        //calculate path
+        string save_path = string("../") + string(DIRECTORY) + string("/temp/") + to_string(piece_index++) + string(IMAGE_FORMAT);
+        //save the file
+        imwrite(save_path,piece_no_hole);
 
     }
 }
 
+// return if a mask is convex or not,
+// it the function calculate the "convex percentage"
+// witch is defined as the number of pixels of the original image decided by
+// the number of pixels in the convex hull of the image
+bool is_convex(Mat &input, float min_percentage = 0.9){
+    Mat convex_hull;
+    quick_convex_hull(input,convex_hull);
+    return (float) countNonZero(input)/(float) countNonZero(convex_hull) > min_percentage;
+}
 
 // find the convex hull of a mask, and fill it with white.
 void quick_convex_hull(Mat &input, Mat &output){
@@ -245,6 +332,8 @@ void quick_convex_hull(Mat &input, Mat &output){
         drawContours(output, hull, (int)i, 255 );
     }
 
+
     floodFill(output,Point(0,0),Scalar(255));
-    output = output != 255;
+    floodFill(output,Point(0,0),Scalar(100));
+    output = output != 100;
 }
