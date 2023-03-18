a2a7cd5da64726b0bd6b1f74c920756e3f4d8117
luca <lucasar.com8@gmail.com>
2023-03-18 11:28:20 +0100

    add definitive data!

--- dataset/dataset_processing.cpp
+++ dataset/dataset_processing.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 using namespace cv;
 
+
+// on my 5700u laptop this function can process around 110 images/minute
 int main(){
 
     int number_of_pieces = split_pieces_into_single_images("../blue_500pcs/raw","../blue_500pcs/divided");
