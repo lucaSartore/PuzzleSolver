a2c8936793bdf7bbf0fa7975f0daf21e5e5b2b56
luca <lucasar.com8@gmail.com>
2023-04-15 09:56:55 +0200

    remove vad bg from images

--- dataset/dataset_processing.cpp
+++ dataset/dataset_processing.cpp
@@ -9,12 +9,16 @@ using namespace cv;
 // on my 5700u laptop this function can process around 110 images/minute
 int main(){
 
+    string path = "../500pcs_V2";
+
     int number_of_pieces = 500;
-    number_of_pieces = split_pieces_into_single_images("../test_2x3/raw","../test_2x3/divided",1200, true);
+    number_of_pieces = split_pieces_into_single_images(path + "/raw",path + "/divided",1200, false);
 
     cout << "i have found " << number_of_pieces << " puzzle pieces" << endl;
 
-    do_pre_processing("../test_2x3/divided",number_of_pieces, 1200, false, true);
+    return 0;
+
+    do_pre_processing(path+"/divided",number_of_pieces, 1200, false, true);
 
     cout << "preprocessing finished" << endl;
 
