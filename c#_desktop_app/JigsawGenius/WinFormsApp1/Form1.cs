a926a51902d4b9902a47561ed8bf7542ce0eab5d
lucaSartore <lucasar.com8@gmail.com>
2023-05-17 11:53:33 +0200

    Add menu bar to App

--- c#_desktop_app/JigsawGenius/WinFormsApp1/Form1.cs
+++ c#_desktop_app/JigsawGenius/WinFormsApp1/Form1.cs
@@ -6,5 +6,35 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gitRepoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
