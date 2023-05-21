2fd1591d799bf4d8ab597ea1b7ad97c544f7d6c8
lucaSartore <lucasar.com8@gmail.com>
2023-05-21 16:37:54 +0200

    add missing files from git

---
+++ c#_desktop_app/JigsawGenius/WinFormsApp1/ThresholdTweaking.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JigsawGenius
+{
+
+    public partial class ThresholdTweaking : Form
+    {
+        private Comunicator _comunicator;
+
+        public ThresholdTweaking(Comunicator comunicator)
+        {
+            _comunicator = comunicator;
+            InitializeComponent();
+            // click the button once to create image preview
+            _testButton_Click(null, null);
+
+            // make it not resizable
+            this.MinimumSize = new System.Drawing.Size(1158, 865);
+            this.MaximumSize = new System.Drawing.Size(1158, 865);
+        }
+
+        private void _testButton_Click(object sender, EventArgs e)
+        {
+            _comunicator.SetSplitThreshold((byte)thresholdTrackBar.Value);
+            pictureBox1.Image = _comunicator.GetThresholdPreview();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
