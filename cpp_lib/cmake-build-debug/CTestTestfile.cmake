447a4c424dbcef6f39e33798dd059caacd0817ce
lucas <lucasar.com8@gmail.com>
2023-05-23 13:51:05 +0200

    update gitignore and delete files

--- cpp_lib/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/lucas/CLionProjects/Puzzle_Solver/cpp_lib
-# Build directory: C:/Users/lucas/CLionProjects/Puzzle_Solver/cpp_lib/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[PuzzleSolverTest]=] "C:/Users/lucas/CLionProjects/Puzzle_Solver/cpp_lib/cmake-build-debug/PuzzleSolverTest.exe")
-set_tests_properties([=[PuzzleSolverTest]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/lucas/CLionProjects/Puzzle_Solver/cpp_lib/CMakeLists.txt;83;add_test;C:/Users/lucas/CLionProjects/Puzzle_Solver/cpp_lib/CMakeLists.txt;0;")
