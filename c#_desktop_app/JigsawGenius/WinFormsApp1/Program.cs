a6b60de6c31283d3a3baf7f54ebe75b3dc7bf8d7
lucaSartore <lucasar.com8@gmail.com>
2023-05-28 20:48:22 +0200

    fix margins

--- c#_desktop_app/JigsawGenius/WinFormsApp1/Program.cs
+++ c#_desktop_app/JigsawGenius/WinFormsApp1/Program.cs
@@ -12,8 +12,8 @@ namespace JigsawGenius
         static void Main()
         {
 
-            //Comunicator c = new comunicator("c:\\users\\lucas\\clionprojects\\puzzle_solver\\cpp_lib\\work_path\\status.txt");
-            //return;
+            Comunicator c = new Comunicator("c:\\users\\lucas\\clionprojects\\puzzle_solver\\cpp_lib\\work_path\\");
+            return;
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
