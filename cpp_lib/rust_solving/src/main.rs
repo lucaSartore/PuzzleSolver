472f07ac3351a4f24e2b736ed9620d71e01f2599
lucaSartore <lucasar.com8@gmail.com>
2023-06-27 15:33:38 +0200

    backup commit

--- cpp_lib/rust_solving/src/main.rs
+++ cpp_lib/rust_solving/src/main.rs
@@ -1,5 +1,7 @@
 mod shore;
 
+use std::collections::HashSet;
+use std::process::id;
 use Direction::DOWN;
 use shore::Shore;
 
@@ -10,39 +12,54 @@ pub enum Direction {UP, RIGHT, DOWN, LEFT}
 pub trait Comparable{
     /// compare this piece two another piece from an orientation. the recursive orientations are needed to keep track of
     /// how many times the pieces has been rotated
-    fn compare_to(&self, direction: Direction,other: &Self, recursive_orientation: usize, recursive_orientation_other: usize) -> Shore{
-        return Shore::new();
-    }
+    fn compare_to(&self, direction: Direction,other: &Self, recursive_orientation: usize, recursive_orientation_other: usize) -> Shore;
 }
 
+/// trait for all the level that has an hash set in ti (all except the first)
+pub trait HasSetInIt{
+    /// get a set containing the ids of the current set
+    fn get_ids(&self) -> &HashSet<usize>;
+}
 
 /// one single piece, keep the ID and his rotation
-struct SingePiece{
+pub struct SingePiece{
     id: usize,
     orientation: usize
+}
+
+impl SingePiece {
+    pub fn get_id(&self) -> usize{
+        self.id
+    }
+    pub fn get_orientation(&self) -> usize{
+        self.orientation
+    }
+    pub fn new(id: usize, orientation: usize) -> Self{
+        assert!(orientation < 4);
+        Self{
+            id,
+            orientation
+        }
+    }
 }
 
 impl Comparable for SingePiece{
     fn compare_to(&self, direction: Direction,other: &Self, recursive_orientation: usize, recursive_orientation_other: usize) -> Shore{
         return Shore::new();
     }
+
 }
 
-struct PieceGroup<'a,T: Comparable>{
-//struct PieceGroup<'a,T>{
+pub struct PieceGroup<'a,T: Comparable>{
     pub pieces: [&'a T;4],
-    orientation: usize
+    orientation: usize,
+    ids: HashSet<usize>
 }
 
-/// when trying to create a new group of piece many things can go wrong, this enum is the result
-/// that represent all the errors, as well as the solution
-pub enum GroupCreationResult<'a, T: Comparable>{
-    Ok(PieceGroup<'a, T>),
-    TopRightImpossibleFit,
-    RopLeftImpossibleFit,
-    BottomLeftImpossibleFit,
-    BottomRightImpossibleFit
-
+impl<'a, T: Comparable> HasSetInIt for PieceGroup<'a, T> {
+    fn get_ids(&self) -> &HashSet<usize> {
+        &self.ids
+    }
 }
 
 /// functions to get a specific sub piece of the group
@@ -72,17 +89,94 @@ impl<'a, T: Comparable> PieceGroup<'a, T> {
     fn set_bottom_right(&mut self, to_set: &'a T){
         self.pieces[(3+self.orientation)%4] = to_set;
     }
+}
 
-    /// create a new group of pieces with the 4 sub pieces
-    fn new<'b>(top_left: &'b T, top_right: &'b T, bottom_right: &'b T, bottom_left: &'b T) -> PieceGroup<'b,T>{
-        // create the object itself
-        let n = PieceGroup::<'b,T>{
-            pieces: [top_left,top_right,bottom_left,bottom_left],
-            orientation: 0
+/// implementation of the new function for the second and above levels
+impl<'a,T: Comparable + HasSetInIt> PieceGroup<'a,T> {
+
+    fn new(top_left: &'a T, top_right: &'a T, bottom_right: &'a T, bottom_left: &'a T) -> GroupCreationResult<'a,T>{
+
+        let top_left_ids = top_left.get_ids();
+        let top_right_ids = top_right.get_ids();
+
+        //if the two pieces has an element in common the group is invalid
+        if !top_left_ids.is_disjoint(top_right_ids) {
+            return GroupCreationResult::TopRightImpossibleCombination;
+        }
+
+        // create the set of the group we are creating
+        let mut ids = top_left_ids.clone();
+        ids.extend(top_right_ids);
+
+        let bottom_right_ids = bottom_right.get_ids();
+
+        //if the two pieces has an element in common the group is invalid
+        if !ids.is_disjoint(bottom_right_ids) {
+            return GroupCreationResult::BottomRightImpossibleCombination;
+        }
+
+        //create the set of the group we are creating
+        ids.extend(top_right_ids);
+
+        let bottom_left_ids = bottom_left.get_ids();
+
+        //if the two pieces has an element in common the group is invalid
+        if !ids.is_disjoint(bottom_left_ids) {
+            return GroupCreationResult::BottomLeftImpossibleCombination;
+        }
+
+        //create the set of the group we are creating
+        ids.extend(bottom_left_ids);
+
+        // create the return object
+        let ret = Self{
+            pieces: [top_left, top_right,bottom_right, bottom_left],
+            orientation: 0,
+            ids
+        };
+        todo!()
+    }
+
+}
+/// implementation of the new function for the first and above levels
+impl<'a> PieceGroup<'a,SingePiece> {
+
+    fn new(top_left: &'a SingePiece, top_right: &'a SingePiece, bottom_right: &'a SingePiece, bottom_left: &'a SingePiece) -> GroupCreationResult<'a,SingePiece>{
+
+        //if they have the same id, return an error
+        if top_left.get_id() == top_right.get_id(){
+            return GroupCreationResult::TopRightImpossibleCombination;
+        }
+
+        //if they have the same id, return an error
+        if top_left.get_id() == bottom_right.get_id() ||  top_right.get_id() == bottom_right.get_id(){
+            return GroupCreationResult::BottomRightImpossibleCombination;
+        }
+
+        //if they have the same id, return an error
+        if top_left.get_id() == bottom_left.get_id() ||  top_right.get_id() == bottom_left.get_id() || bottom_right.get_id() == bottom_left.get_id(){
+            return GroupCreationResult::BottomRightImpossibleCombination;
+        }
+
+        let ids = HashSet::from(
+            [
+                top_left.get_id(),
+                top_right.get_id(),
+                bottom_right.get_id(),
+                bottom_left.get_id()
+            ]
+        );
+
+        // create the return object
+        let ret = PieceGroup::<'a,SingePiece>{
+            pieces: [top_left, top_right,bottom_right, bottom_left],
+            orientation: 0,
+            ids
         };
 
+        todo!();// calculate the shore
 
-        n
+        return GroupCreationResult::Ok(ret);
     }
 
 }
@@ -189,6 +283,30 @@ impl<'a, T:Comparable> Comparable for PieceGroup<'a, T> {
 }
 
 
+/// when trying to create a new group of piece many things can go wrong, this enum is the result
+/// that represent all the errors, as well as the solution
+pub enum GroupCreationResult<'a, T: Comparable>{
+    /// created successfully, and return the type
+    Ok(PieceGroup<'a, T>),
+    /// the top right piece contain a piece that is also contained in one of the previous pieces
+    TopRightImpossibleCombination,
+    /// the bottom left piece contain a piece that is also contained in one of the previous pieces
+    BottomLeftImpossibleCombination,
+    /// the bottom right piece contain a piece that is also contained in one of the previous pieces
+    BottomRightImpossibleCombination,
+    /// the top right piece dose not fit with the preius piece
+    TopRightImpossibleFit,
+    /// the bottom left piece dose not fit with the preius piece
+    BottomLeftImpossibleFit,
+    /// the bottom right piece dose not fit with the preius piece
+    BottomRightImpossibleFit
+}
+
+/// when calculating a shore of a group of pieces, this enum will be return to
+pub enum CalculateShoreResult{
+
+}
+
 impl Comparable for i32 {
     fn compare_to(&self, direction: Direction, other: &Self, recursive_orientation: usize, recursive_orientation_other: usize) -> Shore {
         todo!()
@@ -200,7 +318,8 @@ fn main(){
 
     let pg = PieceGroup::<i32>{
         pieces: [&1,&2,&3,&4],
-        orientation: 0
+        orientation: 0,
+        ids: HashSet::default()
     };
 
     pg.compare_to(DOWN,&pg,0,0);
